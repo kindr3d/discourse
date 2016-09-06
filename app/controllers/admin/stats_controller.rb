59b5bb82bb9e82cfd7a203d3372a271980e72fa5
hom3mad3 <micaelarava@gmail.com>
2016-09-06 17:10:51 +0200

    stats_controller added totals and comparisons for all 4 stats/components

--- app/controllers/admin/stats_controller.rb
+++ app/controllers/admin/stats_controller.rb
@@ -6,22 +6,35 @@ class Admin::StatsController < ApplicationController
 
       time_unit = case time
       when "today"
-        1.day.ago
+        {current: 1.day.ago, previous: 2.days.ago}
       when "week"
-        1.week.ago
+        {current: 1.week.ago, previous: 2.weeks.ago}
       when "month"
-        4.weeks.ago
+        {current: 4.weeks.ago, previous: 8.weeks.ago}
       else
         raise "Invalid time parameter"
       end
-      puts time_unit
 
 
+      # function that counts total new elements within time unit
+      def total_count(target_model, column, time_limit)
+        total = target_model.where("#{column} > ?", time_limit[:current]).count.to_f #active record
+        total_last = target_model.where("#{column} > ?", time_limit[:last]).count.to_f #active record
+        if total_last > 0 #avoid dividing by 0
+          compare = ((total/total_last -1) *100).round(1)
+        end
+        return {total: total, compare_percent: compare}
+      end
 
-  
+
+      #define models
+      visit_count = total_count UserVisit, "visited_at", time_unit
+      user_count = total_count User, "created_at", time_unit
+      topic_count = total_count Topic, "created_at", time_unit
+      post_count = total_count Post, "created_at", time_unit
 
 
-      render json: { name: "donut", description: "delicious!", time: time, time_u: time_unit }
+      render json: { visit_title: "Visits", time: time, time_u: time_unit, visit_total: visit_count, user_total: user_count, topic_total: topic_count}
     end
 
 end
